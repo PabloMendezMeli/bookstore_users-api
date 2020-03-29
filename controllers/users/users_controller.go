bc82e81883accb7d839a7c8ce4bd22c431ddbdc1
Pablo Mendez <pablo.mendez@mercadolibre.com>
2020-03-29 12:07:28 -0300

    changes for starting from our domain chapter

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,32 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/PabloMendezMeli/bookstore_users-api/domain/users"
+	"github.com/PabloMendezMeli/bookstore_users-api/services"
+	"github.com/PabloMendezMeli/bookstore_users-api/utils/errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
