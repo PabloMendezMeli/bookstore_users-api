d48b124ef3f17b36da0212d6ea40b530c6b12fbc
Pablo Mendez <pablo.mendez@mercadolibre.com>
2020-05-07 17:47:16 -0300

    changes for chapter

--- services/users_service.go
+++ services/users_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/PabloMendezMeli/bookstore_users-api/domain/users"
+	"github.com/PabloMendezMeli/bookstore_users-api/utils/date_utils"
 	"github.com/PabloMendezMeli/bookstore_users-api/utils/errors"
 )
 
@@ -18,6 +19,8 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 		return nil, err
 	}
 
+	user.Status = users.StatusActive
+	user.DateCreated = date_utils.GetNowDbFormat()
 	if err := user.Save(); err != nil {
 		return nil, err
 	}
@@ -59,4 +62,9 @@ func UpdateUser(isPartial bool, user users.User) (*users.User, *errors.RestErr) 
 func DeleteUser(userId int64) *errors.RestErr {
 	user := &users.User{Id: userId}
 	return user.Delete()
+}
+
+func Search(status string) ([]users.User, *errors.RestErr) {
+	dao := users.User{}
+	return dao.FindByStatus(status)
 }
